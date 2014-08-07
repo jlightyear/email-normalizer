1cdc093eab19d91f212e46b95467e1fd80a01f68
jlightyear <joseluis.estrach@gmail.com>
2014-08-07 15:28:40 +0200

    green

---
+++ test.js
@@ -0,0 +1,26 @@
+
+context = describe;
+
+clear_email = function(input){
+	return input;
+}
+
+describe("Email parser", function(){
+
+	/*EMAILS:
+
+	joseluis.estrach(AT)gmail.com => joseluis.estrach@gmail.com
+	joseluis.estrach(AT)gmail(DOT)com => joseluis.estrach@gmail.com
+	joseluis.estrach-AT-gmail-DOT-com => joseluis.estrach@gmail.com
+
+	SAMPLE INPUT:
+	I want to send an email to joseluis.estrach(AT)gmail(DOT)com
+
+	OUTPUT:
+	I want to send an email to joseluis.estrach@gmail.com*/
+
+  it("returns the same string if there is no email", function(){
+    var input_string = "AT"
+    expect(clear_email(input_string)).toBe("AT");
+  });
+})
